0eae740245f23e85c70bbed478200be02396ac13
MrYes2020 <64986162+mryes2020@users.noreply.github.com>
2020-10-23 13:50:59 -0400

    Update impersonner.py

--- impersonner.py
+++ impersonner.py
@@ -24,9 +24,6 @@ print ("WARNING!!!!!!! You have to run me as a root if u havent done it yet!")
 print ("\n------------------")
 print ("[0.5] i dont have the required file installed") 
 print ("[1] normal impersonner") 
-print ("[2] impersonner to lots of people")
-print ("[3] impersonner with proxy !!!!not in the program for now ") 
-print ("[4] impersonner to lots of people with proxy !!!!not in the program for now ") 
 print ("[0] exit boiiiii")
 print ("\n------------------")
 
@@ -49,45 +46,16 @@ if (choice == "0.5"):
 	print ("------------------------")
 elif (choice == "1"):
 	clearscreen()
-	#so, the user's choice is the normal one (in this program it is the impersonner only one user with no proxy
-	print ("You choosed the normal mode with no proxy and only one target!")
+	print ("You choosed the normal mode!")
 	email = input("Enter here the target's email: ")
 	impersonneremail = input("Enter here the email of the sender: ")
 	subject = input("Enter here the subject: ") 
 	body = input("Enter here the body: ") 
-	
 	#kk now we have everything we need to start the attack 
 	clearscreen()
 	print("Sending the mail")
 	os.system("echo " + "'" + body + "'" + " | mail -s " + "'" + subject + "'" + " -a 'From: " + impersonneremail + "' " + email)
 	print("Email sent")
-elif (choice == "2"):
-	clearscreen() 
-	print ("You have choosed the 2. mode with no proxy but sending the email to multiple targets!")
-	emails = input("Enter here emails of all the targets [emails should be seperated with a , example: something@example.com,ye@no.com]: ")
-	email = input("Enter here the email of the sender: ") 
-	subject = input("Enter here the subject of the message: ") 
-	body = input("Enter here the body of the message: ") 
-	
-	#kk now we have all the necessary input from the user and we just have to seperate the emails and send email to each of those on the list boi
-	clearscreen()
-
-#f*ck this code man 
-#	emailsarray = emails.split(",")
-#	for targetemail in emailsarray:
-#		print (targetemail) 
-#		os.system("echo " + "'" + body + "'" + " | mail -s " + "'" + subject + "'" + " -a 'From: " + email + "'"  + targetemail)
-	emailslist = emails.split(",")
-	targets = ""
-	for semail in emailslist: 
-		targets = targets + " " + semail
-	os.system("echo " + "'" + body + "'" + " | mail -s " + "'" + subject + "'" + " -a 'From: " + email + "'"  + " " + targets)
-elif (choice == "3"):
-	clearscreen()
-	print ("This mode is not in the program for now!!") 
-elif (choice == "4"):
-	clearscreen()
-	print ("This mode is not in the program for now!!") 
 elif (choice == "0"):
 	clearscreen()
 	print ("KK goodbye boiiii see u") 
